8e952bd259f57f08e6327179acac1f2c852d5063
Lucas Luiz Rocha <115369609+LUCAS-LUIZ-ROCHA@users.noreply.github.com>
2023-01-28 18:16:25 -0300

    Add files via upload
    Mais atualizações na ia Cameline.

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import speech_recognition as sr
 import pyttsx3
+import datetime
+import wikipedia
+import pywhatkit
 
-audio = sr.recognizer()
+
+audio = sr.Recognizer()
 maquina = pyttsx3.init()
-maquina.say("Ola, eu sou a Cameline")
-maquina.say("Como posso ajudar?")
-maquina.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print("Ouvindo...")
-        voz = audio.listen(source)
-        comando = audio.recognizer_google(voz, language='pt-BR')
-        comando = comando.lower()
-        if 'cameline' in comando:
-            print(comando)
-except:
-    print("Microfone está desligado")
+
+def executa_comando():
+    try:
+        with sr.Microphone() as source:
+            print("Ouvindo..")
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language="pt-BR")
+            comando = comando.lower()
+            if "cameline" in comando:
+                comando = comando.replace("cameline", "")
+                maquina.say(comando)
+                maquina.runAndWait()
+
+    except:
+        print("Microfone não está ok")
+
+    return comando
+
+
+def comando_voz_usuario():
+    comando = executa_comando()
+    if "horas" in comando:
+        hora = datetime.datetime.now().strftime("%H:%M")
+        maquina.say("Agora são" + hora)
+        maquina.runAndWait()
+    elif "procure por" in comando:
+        procurar = comando.replace("procure por", "")
+        wikipedia.set_lang("pt")
+        resultado = wikipedia.summary(procurar, 2)
+        print(resultado)
+        maquina.say(resultado)
+        maquina.runAndWait()
+    elif "toque" in comando:
+        musica = comando.replace("toque", "")
+        resultado = pywhatkit.playonyt(musica)
+        maquina.say("Tocando música")
+        maquina.runAndWait()
+
+
+comando_voz_usuario()
+
+
+
